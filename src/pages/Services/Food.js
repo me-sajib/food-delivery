13d80c7146424fdca32553efce2393667dba3ea9
sohel0199 <sohel019954@gmail.com>
2023-02-05 18:25:01 +0600

    product related item is showed on tha products details page

--- src/pages/Services/Food.js
+++ src/pages/Services/Food.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Food = (props) => {
-	const { id, name, image, category } = props.food;
+	const { id, name, image, category, location } = props.food;
 	return (
-		<div className="col">
+		<div data-aos="zoom-in" data-aos-duration="2000" className="col">
 			<div className="card">
 				<div className="card-img">
 					<img src={image} className="card-img-top" alt="food" />
@@ -16,7 +16,11 @@ const Food = (props) => {
 
 					<ul>
 						<li>{category}</li>
+
 					</ul>
+					<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+						<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+					</svg>{location}</p>
 				</div>
 			</div>
 		</div>
--- src/pages/Services/ShowProductByCategory.js
+++ src/pages/Services/ShowProductByCategory.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Food from "./Food";
 
+
 export default function ShowProductByCategory({ type, id }) {
 	const [foods, setFoods] = useState([]);
 	const [allFood, setAllFood] = useState([]);
@@ -30,7 +31,8 @@ export default function ShowProductByCategory({ type, id }) {
 
 	return (
 		<div className="my-5">
-			<h3>Related Item</h3>
+			{foods?.length > 0 ? <h3>Related Item</h3> : null}
+
 			<div className="row row-cols-1 row-cols-md-3 g-4">
 				{foods.map((fd) => (
 					<Food food={fd} key={fd.id} />
