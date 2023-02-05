9c16c6c822c7cb850d9a634c4af3abb6b7044be1
shohagroy <pkshohag240@gmail.com>
2023-02-05 20:34:26 +0600

    new registration form added

--- src/pages/Services/ShowProductByCategory.js
+++ src/pages/Services/ShowProductByCategory.js
@@ -2,40 +2,40 @@ import React, { useState, useEffect } from "react";
 import Food from "./Food";
 
 export default function ShowProductByCategory({ type, id }) {
-	const [foods, setFoods] = useState([]);
-	const [allFood, setAllFood] = useState([]);
-	const [related, setRelated] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [allFood, setAllFood] = useState([]);
+  const [related, setRelated] = useState([]);
 
-	useEffect(() => {
-		fetch("food.json")
-			.then((res) => res.json())
-			.then((data) => {
-				setAllFood(data);
-			});
-	}, [type, id]);
+  useEffect(() => {
+    fetch("food.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllFood(data);
+      });
+  }, [type, id]);
 
-	useEffect(() => {
-		const relateds = allFood?.filter((fd) => fd?.type === type);
-		console.log(relateds);
-		setRelated(relateds);
-	}, [type]);
+  useEffect(() => {
+    const relateds = allFood?.filter((fd) => fd?.type === type);
+    console.log(relateds);
+    setRelated(relateds);
+  }, [type]);
 
-	useEffect(() => {
-		if (related.length > 0) {
-			const sameItem = related?.filter((fd) => fd.id != id);
-			setFoods(sameItem);
-			console.log("hello", sameItem);
-		}
-	}, [id, related]);
+  useEffect(() => {
+    if (related.length > 0) {
+      const sameItem = related?.filter((fd) => fd.id != id);
+      setFoods(sameItem);
+      console.log("hello", sameItem);
+    }
+  }, [id, related]);
 
-	return (
-		<div className="my-5">
-			<h3>Related Item</h3>
-			<div className="row row-cols-1 row-cols-md-3 g-4">
-				{foods.map((fd) => (
-					<Food food={fd} key={fd.id} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="my-5">
+      <h3>Related Item</h3>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {foods.map((fd) => (
+          <Food food={fd} key={fd.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -2,176 +2,257 @@ import { updateProfile } from "firebase/auth";
 import React from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../firebase.config";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FaStarOfLife } from "react-icons/fa";
+import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs";
 
 const Register = () => {
-	const [createUserWithEmailAndPassword, user, loading, error] =
-		useCreateUserWithEmailAndPassword(auth);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
 
-	const navigateToHome = useNavigate();
+  const navigateToHome = useNavigate();
 
-	const registerUser = (e) => {
-		e.preventDefault();
-		const firstName = e.target.firstName.value;
-		const lastName = e.target.lastName.value;
-		const email = e.target.email.value;
-		const password = e.target.password.value;
-		createUserWithEmailAndPassword(email, password);
-		updateProfile(auth, { displayName: firstName });
-	};
-	if (error) {
-		console.log(error);
-	}
+  const registerUser = (e) => {
+    e.preventDefault();
+    const firstName = e.target.firstName.value;
+    const lastName = e.target.lastName.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+    updateProfile(auth, { displayName: firstName });
+  };
+  if (error) {
+    console.log(error);
+  }
 
-	// useEffect(() => {
-	//   navigateToHome("/");
-	// }, [navigateToHome]);
-	if (user) {
-		navigateToHome("/");
-	}
+  // useEffect(() => {
+  //   navigateToHome("/");
+  // }, [navigateToHome]);
+  if (user) {
+    navigateToHome("/");
+  }
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div className="container">
-			<div className="py-5">
-				<h1 className="text-warning text-center py-5">
-					Become a our member? Register now
-				</h1>
-				<form className="form-width" onSubmit={registerUser}>
-					<div className="row mb-4">
-						<div className="col">
-							<div className="form-outline">
-								<input
-									type="text"
-									id="form3Example1"
-									placeholder="first name"
-									className="form-control"
-									name="firstName"
-								/>
-								<label
-									className="form-label"
-									for="form3Example1"
-								>
-									First name
-								</label>
-							</div>
-						</div>
-						<div className="col">
-							<div className="form-outline">
-								<input
-									type="text"
-									placeholder="last name"
-									id="form3Example2"
-									className="form-control"
-									name="lastName"
-								/>
-								<label
-									className="form-label"
-									for="form3Example2"
-								>
-									Last name
-								</label>
-							</div>
-						</div>
-					</div>
+  return (
+    <div className="container">
+      <div className="py-5">
+        <h1 className="text-warning text-center py-5">
+          Become a our member? Register now
+        </h1>
+        {/* <form className="form-width" onSubmit={registerUser}>
+          <div className="row mb-4">
+            <div className="col">
+              <div className="form-outline">
+                <input
+                  type="text"
+                  id="form3Example1"
+                  placeholder="first name"
+                  className="form-control"
+                  name="firstName"
+                />
+                <label className="form-label" for="form3Example1">
+                  First name
+                </label>
+              </div>
+            </div>
+            <div className="col">
+              <div className="form-outline">
+                <input
+                  type="text"
+                  placeholder="last name"
+                  id="form3Example2"
+                  className="form-control"
+                  name="lastName"
+                />
+                <label className="form-label" for="form3Example2">
+                  Last name
+                </label>
+              </div>
+            </div>
+          </div>
 
-					<div className="form-outline mb-4">
-						<input
-							type="number"
-							placeholder="example: 01XXXXXXXXX"
-							id="form3Example33"
-							className="form-control"
-							name="phone"
-						/>
-						<label className="form-label" for="form3Example33">
-							Phone Number
-						</label>
-					</div>
+          <div className="form-outline mb-4">
+            <input
+              type="number"
+              placeholder="example: 01XXXXXXXXX"
+              id="form3Example33"
+              className="form-control"
+              name="phone"
+            />
+            <label className="form-label" for="form3Example33">
+              Phone Number
+            </label>
+          </div>
 
-					<div className="form-outline mb-4">
-						<input
-							type="email"
-							placeholder="enter your email"
-							id="form3Example3"
-							className="form-control"
-							name="email"
-						/>
-						<label className="form-label" for="form3Example3">
-							Email address
-						</label>
-					</div>
+          <div className="form-outline mb-4">
+            <input
+              type="email"
+              placeholder="enter your email"
+              id="form3Example3"
+              className="form-control"
+              name="email"
+            />
+            <label className="form-label" for="form3Example3">
+              Email address
+            </label>
+          </div>
 
-					<div className="form-outline mb-4">
-						<input
-							type="password"
-							placeholder="enter your secure password"
-							id="form3Example4"
-							className="form-control"
-							name="password"
-						/>
-						<label className="form-label" for="form3Example4">
-							Password
-						</label>
-					</div>
+          <div className="form-outline mb-4">
+            <input
+              type="password"
+              placeholder="enter your secure password"
+              id="form3Example4"
+              className="form-control"
+              name="password"
+            />
+            <label className="form-label" for="form3Example4">
+              Password
+            </label>
+          </div>
 
-					<div className="form outline mb-4">
-						<select
-							name="role"
-							id="user"
-							className="form-select"
-							aria-label="Default select"
-						>
-							<option selected>User</option>
-						</select>{" "}
-						<label className="form-label" for="user">
-							Role
-						</label>
-					</div>
+          <div className="form outline mb-4">
+            <select
+              name="role"
+              id="user"
+              className="form-select"
+              aria-label="Default select"
+            >
+              <option selected>User</option>
+            </select>{" "}
+            <label className="form-label" for="user">
+              Role
+            </label>
+          </div>
 
-					<button
-						type="submit"
-						className="btn btn-primary btn-block mb-4"
-					>
-						Sign up
-					</button>
+          <button type="submit" className="btn btn-primary btn-block mb-4">
+            Sign up
+          </button>
 
-					<div className="text-center">
-						<p>or sign up with:</p>
-						<button
-							type="button"
-							className="btn btn-primary btn-floating mx-1"
-						>
-							<i className="fab fa-facebook-f"></i>
-						</button>
+          <div className="text-center">
+            <p>or sign up with:</p>
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+              <i className="fab fa-facebook-f"></i>
+            </button>
 
-						<button
-							type="button"
-							className="btn btn-primary btn-floating mx-1"
-						>
-							<i className="fab fa-google"></i>
-						</button>
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+              <i className="fab fa-google"></i>
+            </button>
 
-						<button
-							type="button"
-							className="btn btn-primary btn-floating mx-1"
-						>
-							<i className="fab fa-twitter"></i>
-						</button>
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+              <i className="fab fa-twitter"></i>
+            </button>
 
-						<button
-							type="button"
-							className="btn btn-primary btn-floating mx-1"
-						>
-							<i className="fab fa-github"></i>
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+            <button type="button" className="btn btn-primary btn-floating mx-1">
+              <i className="fab fa-github"></i>
+            </button>
+          </div>
+        </form> */}
+
+        <form action="" className="form-width">
+          <div className="row  ">
+            <div className="col container ">
+              <div className="form-floating">
+                <input
+                  type="email"
+                  className="form-control "
+                  placeholder="name@example.com"
+                  id="floatingInputGrid"
+                />
+                <label for="floatingInputGrid">
+                  Full Name{" "}
+                  <span>
+                    <FaStarOfLife size={10} className="text-danger mb-2 " />
+                  </span>
+                </label>
+
+                <div className="form-floating mt-2">
+                  <input
+                    type="tel"
+                    className="form-control"
+                    placeholder="name@example.com"
+                    id="floatingInputGrid"
+                  />
+                  <label for="floatingInputGrid">
+                    Mobile Number{" "}
+                    <span>
+                      <FaStarOfLife size={10} className="text-danger mb-2 " />
+                    </span>
+                  </label>
+                </div>
+
+                <div className="form-floating mt-2">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="name@example.com"
+                    id="floatingInputGrid"
+                  />
+                  <label for="floatingInputGrid">
+                    Address{" "}
+                    <span>
+                      <FaStarOfLife size={10} className="text-danger mb-2 " />
+                    </span>
+                  </label>
+                </div>
+
+                <div className="form-floating mt-2">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="name@example.com"
+                    id="floatingInputGrid"
+                  />
+                  <label for="floatingInputGrid">
+                    Email Address{" "}
+                    <span>
+                      <FaStarOfLife size={10} className="text-danger mb-2 " />
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="form-floating mt-2">
+              <select className="form-select" id="floatingSelectGrid">
+                <option selected>Select Your Role</option>
+                <option value="user">User</option>
+              </select>
+              <label for="floatingInputGrid">
+                Your Role
+                <span>
+                  <FaStarOfLife size={10} className="text-danger mb-2 " />
+                </span>
+              </label>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary  my-4">
+            Register
+          </button>
+        </form>
+
+        <div className="d-flex justify-content-center align-items-baseline">
+          <p>Already have an Account? </p>
+          <Link to="/login">Login</Link>
+        </div>
+
+        <div className="text-center mt-4">
+          <p className=" ">Or Register with</p>
+          <button type="button" className="btn btn-primary btn-floating mx-1">
+            <BsGoogle size={20} />
+          </button>
+
+          <button type="button" className="btn btn-primary btn-floating mx-1">
+            <BsFacebook size={20} />
+          </button>
+          <button type="button" className="btn btn-primary btn-floating mx-1">
+            <BsTwitter size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
