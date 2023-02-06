c6ebcb6a1ce83c1dc80801adc84544fb09e13070
AquiLiusOP <adibmannanrafi2003@gmail.com>
2023-02-06 21:39:32 +0600

    became a seller page added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import Login from "./pages/auth/Login";
-import Nav from "./components/Nav";
-import Register from "./pages/auth/Register";
-import Footer from "./components/Footer";
-import FoodById from "./pages/Services/FoodById";
 import PrivateRoute from "./Auth/PrivateRoute";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import BecameASeller from "./pages/BecameASeller/BecameASeller";
+import FoodById from "./pages/Services/FoodById";
 
 function App() {
-	return (
-		<div style={{ width: "100%" }}>
-			<Nav />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route
-					path="/:id"
-					element={
-						<PrivateRoute>
-							<FoodById />
-						</PrivateRoute>
-					}
-				/>
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div style={{ width: "100%" }}>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/:id"
+          element={
+            <PrivateRoute>
+              <FoodById />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/became-a-seller"
+          element={
+            <PrivateRoute>
+              <BecameASeller />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
