5a8ab6e8c00dfed0d6cfa02286725cc0bec5b1b1
AquiLiusOP <adibmannanrafi2003@gmail.com>
2023-02-07 22:46:11 +0600

    became a seller page added

---
+++ src/pages/BecameASeller/BecameASeller.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+const BecameASeller = () => {
+  return (
+    <div className="m-5 pb-5">
+      <form className="row g-3 needs-validation" noValidate>
+        <div className="col-md-4">
+          <label htmlFor="validationCustom01" className="form-label">
+            First name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustom01"
+            required
+          />
+          <div className="valid-feedback">Looks good!</div>
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="validationCustom02" className="form-label">
+            Last name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustom02"
+            required
+          />
+          <div className="valid-feedback">Looks good!</div>
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="validationCustomUsername" className="form-label">
+            Email
+          </label>
+          <div className="input-group has-validation">
+            <input
+              type="text"
+              className="form-control"
+              id="validationCustomUsername"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <div className="invalid-feedback">Please choose a email.</div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="validationCustom03" className="form-label">
+            City
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustom03"
+            required
+          />
+          <div className="invalid-feedback">Please provide a valid city.</div>
+        </div>
+        <div className="col-md-3">
+          <label htmlFor="validationCustom05" className="form-label">
+            Zip
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustom05"
+            required
+          />
+          <div className="invalid-feedback">Please provide a valid zip.</div>
+        </div>
+        <div className="col-md-3">
+          <label htmlFor="validationCustom05" className="form-label">
+            Picture URL
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustom05"
+            required
+          />
+          <div className="invalid-feedback">Please provide a valid zip.</div>
+        </div>
+        <div className="col-12">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="invalidCheck"
+              required
+            />
+            <label className="form-check-label" htmlFor="invalidCheck">
+              Agree to terms and conditions
+            </label>
+            <div className="invalid-feedback">
+              You must agree before submitting.
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default BecameASeller;
