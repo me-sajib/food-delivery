b3b31e8f26b0765dc47c2d6cead7224e5a02d197
me-sajib <me.mrsajib@gmail.com>
2023-02-22 21:17:58 +0600

    back button add

--- src/pages/BecameADeliveryMan/BecameADeliveryMan.js
+++ src/pages/BecameADeliveryMan/BecameADeliveryMan.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const BecameADeliveryMan = () => {
   const [disabled, setDisabled] = useState(true);
@@ -118,6 +119,7 @@ const BecameADeliveryMan = () => {
           <button className="btn btn-primary" disabled={disabled} type="submit">
             Submit
           </button>
+          <Link to="/" className="btn-link btn">Back</Link>
         </div>
       </form>
     </div>
